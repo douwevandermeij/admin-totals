6e5e076bda75e296a3da50af832d6c3625b3e6af
Douwe van der Meij <douwe@karibu-online.nl>
2017-12-27 21:46:07 +0100

    find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 REQUIREMENTS = (
     'Django',
@@ -16,7 +16,7 @@ setup(
     """,
     long_description=open('README.md', 'rt').read(),
     url="",
-    packages=['admin_totals'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIREMENTS,
     tests_require=TEST_REQUIREMENTS,
