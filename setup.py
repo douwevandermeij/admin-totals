e166c583d231aab0261cc946c902e571c18941ed
Douwe van der Meij <douwe@karibu-online.nl>
2019-01-24 22:45:49 +0100

    added license, enriched setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ REQUIREMENTS = (
     'Django',
 )
 TEST_REQUIREMENTS = (
+    'markdown==2.6.4',
+    'coverage',
+    'flake8',
+    'isort',
+    'mock',
+    'pytz',
+    'unittest-xml-reporting',
 )
 
 
@@ -12,19 +19,21 @@ setup(
     version='1.0',
     author="Douwe van der Meij",
     author_email="douwe@karibu-online.nl",
-    description="""Django Admin Totals.
+    description="""Django Admin Totals, add totals to your columns in Django admin.
     """,
     long_description=open('README.md', 'rt').read(),
-    url="",
+    url="https://github.com/douwevandermeij/admin-totals",
     packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIREMENTS,
     tests_require=TEST_REQUIREMENTS,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
